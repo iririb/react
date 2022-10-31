6506ae2064f919385c5276c7f7c08a68a18ecd60
iririb <eshamibrahim94@gmail.com>
2022-11-01 00:37:46 +0800

    useState hook to update value

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,25 +1,28 @@
+import { useState } from 'react'
+
 const Home = () => {
 
-    // e is referring to event object
-    // without event object, function still working
-    const handleClick = (e) => {
-        console.log("Hello ninja", e)
-    }
-    const handleClickAgain = (name, e) => {
-        console.log('Hello ' + name, e.target)
+    // useState return 2 item, which is variable that store value and function to change those value
+    const [name, setName] = useState('mario')
+    
+    let nameNG = 'iruiru'
+    
+    const [age, setAge] = useState(25)
+
+    const handleClick = () => {
+        setName('luigi')
+        setAge(30)
+        
+        // below method won't update value, thus we use above
+        nameNG = 'irsham'
+
     }
     return (
         <div className="home">
             <h2>Homepage</h2>
-            {/* if we invoke it(contain '()'), it will trigger automatically  at the beginning*/}
-            {/* <button onClick={handleClick()}>Click me</button> */}
+            <p>{name} is {age} years old</p>
+            <p>{nameNG}</p>
             <button onClick={handleClick}>Click me</button>
-
-            {/* below is how we can trigger event without invoking it */}
-            {/* button still working without event object('e') */}
-            <button onClick={(e) => {
-                handleClickAgain('Iruiru', e)
-            }}>Click me again</button>
         </div>
     );
 }
