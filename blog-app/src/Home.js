dbfe4b3a2b0e988e0e85f3488aa104e171074b26
iririb <eshamibrahim94@gmail.com>
2022-11-03 16:51:58 +0800

    fetching the error

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -4,22 +4,34 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
     const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then((data)=>{
-                setBlogs(data)
-                setIsPending(false)
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if(!res.ok){
+                        throw Error('Could not fetch data on DB')
+                    }
+                    return res.json()
+                })
+                .then((data) => {
+                    setBlogs(data)
+                    setIsPending(false)
+                    setError(null)
+                })
+                .catch(err=>{
+                    setError(err.message)
+                    setIsPending(false)
+                })
+        }, 1000)
     }, [])
 
     return (
         <div className="home">
+            { error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!!!"/>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!!!" />}
         </div>
     );
 }
