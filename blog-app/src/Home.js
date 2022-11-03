685812b2e2a7dc558a94e4429f977d388f58135c
iririb <eshamibrahim94@gmail.com>
2022-11-03 13:26:54 +0800

    hosting data using JSON server

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -10,26 +10,18 @@ const Home = () => {
         { title: 'New Journey someone2', body: 'Not start yet3...', author: 'someone', id: 5 }
     ])
 
-    const [name, setName] = useState('namedesu')
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
     }
 
-    // useEffect ran for each render
-    // adding [] as second parameter will prevent useEffect to trigger for second and next render
     useEffect(() => {
         console.log('useEffect ran')
-        console.log(name)
-    }, [name])
-    // adding above state into second argument will run the useEffect when state mentioned triggered
+    }, [])
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!!!" handleDelete={handleDelete} />
-            <button onClick={() => setName('newnamedesu')}>Change name</button>
-            <p>{name}</p>
         </div>
     );
 }
--- note.md
+++ note.md
@@ -1,2 +1,7 @@
 Command to generate project
 > npx create-react-app <project name>
+
+Running json server to host data
+make sure you are in blog-app directory
+make sure data/db.json exist or not create a new one or point to json file you created
+> npx json-server --watch data/db.json --port 8000
