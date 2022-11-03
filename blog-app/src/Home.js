a46c6a0fec4e3eb127a99af0b6b93192f574d39d
iririb <eshamibrahim94@gmail.com>
2022-11-03 16:38:43 +0800

    fetching data from JSON server

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -2,26 +2,22 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'New Journey', body: 'Not start yet...', author: 'Ghost writer', id: 1 },
-        { title: 'New Journey2', body: 'Not start yet2...', author: 'Ghost writer', id: 2 },
-        { title: 'New Journey3', body: 'Not start yet3...', author: 'Ghost writer', id: 3 },
-        { title: 'New Journey someone1', body: 'Not start yet3...', author: 'someone', id: 4 },
-        { title: 'New Journey someone2', body: 'Not start yet3...', author: 'someone', id: 5 }
-    ])
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
+    const [blogs, setBlogs] = useState(null)
 
     useEffect(() => {
-        console.log('useEffect ran')
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then((data)=>{
+                setBlogs(data)
+            })
     }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!!!" handleDelete={handleDelete} />
+            {/* we need to check blogs whether it's null or not else it will become error due to map function cannot handle null object the first application initilize */}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!!!"/>}
         </div>
     );
 }
